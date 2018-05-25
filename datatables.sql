8a8be768f2f723eb7a238025cff336287e158d2b
DucHieu123 <37857549+DucHieu123@users.noreply.github.com>
2018-05-26 00:08:17 +0700

    commit 25/5

---
+++ datatables.sql
@@ -0,0 +1,12 @@
+﻿insert into TYPEACCOUNT
+values(1, 'Admin'),
+(2, 'Driver')
+
+insert into LEVEL
+values(1, 'Admin Root')
+
+insert into ACCOUNT
+values(1, 'admin', 'E10ADC3949BA59ABBE56E057F20F883E', 1, 1)
+
+insert into ADMINMN
+values(1, N'Nguyễn Văn A', 1)
